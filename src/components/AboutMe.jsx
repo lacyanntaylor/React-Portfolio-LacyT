b0ca806ac7b67f5e39a7e2466ee0e5556951a133
Lacy Ann Taylor <lacy.ann.taylor@gmail.com>
2025-02-10 19:59:24 -0600

    testing

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -3,7 +3,7 @@ function AboutMe() {
       <section>
         <h2>Nice Catch!</h2>
         <img src="Nice_Catch.jpg" alt="Your Avatar" width="150" height="150"/>
-        <p>You don't want to miss this big catch! </p>
+        <p>You do not want to miss this big catch! </p>
       </section>
     );
   }
